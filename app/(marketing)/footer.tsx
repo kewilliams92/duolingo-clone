7af560bf5bd8a05d9f44c959c14d9d23db2a8b28
Kevin Williams <kw92@Kevins-MBP.lan>
2024-03-27 20:57:09 -0400

    created homepage, will create sidebar functionality next

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
   return (
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/ar.svg"
+            alt="Arabic"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Arabic
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/en.svg"
+            alt="English"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          English
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/de.svg"
+            alt="German"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          German
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/it.svg"
+            alt="Italian"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/es.svg"
+            alt="Spanish"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+      </div>
     </footer>
   );
 };
