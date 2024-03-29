a4f0913c5d5151416f94496fc7cdaee72671f978
Kevin Williams <kw92@Kevins-MacBook-Pro.local>
2024-03-29 14:19:35 -0400

    created the userprogress schema, created actions for the user-progress, and when transitioning from course to course it will show the user's current course

--- database/schema.ts
+++ database/schema.ts
@@ -1,7 +1,32 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
 });
+
+//this is a many to many relationship with userprogress
+export const coursesRelations = relations(courses, ({ many }) => ({
+  userProgress: many(userProgress),
+}));
+
+export const userProgress = pgTable("user_progress", {
+  userId: text("user_id").primaryKey(),
+  userName: text("user_name").notNull().default("User"),
+  userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
+  activeCourseId: integer("active_course_id").references(() => courses.id, {
+    onDelete: "cascade",
+  }),
+  hearts: integer("hearts").notNull().default(5),
+  points: integer("points").notNull().default(0),
+});
+
+//this is a one to many relationship with an active course
+export const userProgressRelations = relations(userProgress, ({ one }) => ({
+  activeCourse: one(courses, {
+    fields: [userProgress.activeCourseId],
+    references: [courses.id],
+  }),
+}));
