dec469ada2b91b6737a0fdea7d3aeafc7047afe5
Kevin Williams <kw92@Kevins-MBP.lan>
2024-04-04 16:44:47 -0400

    finished adding stripe to the project. users can now purchase unlimited hearts

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,18 +1,24 @@
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/database/queries";
+import { getUserProgress, getUserSubscription } from "@/database/queries";
 import Image from "next/image";
 import { redirect } from "next/navigation";
 import { Items } from "./items";
 
 const ShopPage = async () => {
   const userProgressData = getUserProgress();
-  const [userProgress] = await Promise.all([userProgressData]);
+  const userSubscriptionData = getUserSubscription();
+  const [userProgress, userSubscription] = await Promise.all([
+    userProgressData,
+    userSubscriptionData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
+
+  const isPro = !!userSubscription?.isActive;
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -21,7 +27,7 @@ const ShopPage = async () => {
           activeCourse={userProgress.activeCourse}
           hearts={userProgress.hearts}
           points={userProgress.points}
-          hasActiveSubscription={false} //TODO: handle subscription query later
+          hasActiveSubscription={isPro}
         />
       </StickyWrapper>
       <FeedWrapper>
@@ -36,7 +42,7 @@ const ShopPage = async () => {
           <Items
             hearts={userProgress.hearts}
             points={userProgress.points}
-            hasActiveSubscription={false} //TODO: handle subscription query later
+            hasActiveSubscription={isPro}
           />
         </div>
       </FeedWrapper>
